726c5502bf03dcf358dfb7078bc0308ca458301c
macwatrous <macwatrous@gmail.com>
2018-02-20 18:06:37 -0500

    db connection test

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,7 +1,15 @@
+var server =  require('../../server');
+var client = server.client;
 module.exports = function (app) {
     // set up the routes themselves
     app.get('/create-user', function (req, res) {
-        res.send('Hello world!')
+        client.query('SELECT NOW() as now', (dberr, dbres) => {
+            if (err) {
+                res.send('Hello world!');
+            } else {
+                res.send(dbres.rows[0]);
+            }
+        });
     });
 
     app.post('/login', function (req, res) {
--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 var express = require('express'),
 app = express(),
 port = process.env.PORT || 8080;
+const { Client } = require('pg');
 
+var dbUser = process.env.POSTGRESQL_USER;
+var dbPass = process.env.POSTGRESQL_PASSWORD;
+var dbName = process.env.POSTGRESQL_NAME;
+var dbHost = "";
+var dbPort = "";
+var testFlag = process.env.TEST_FLAG
+if (testFlag == "test"){
+    //test
+    dbHost = process.env.POSTGRESQLTEST_SERVICE_HOST;
+    dbPort = process.env.POSTGRESQLTEST_SERVICE_PORT;    
+    
+} else {
+    //prod
+    dbHost = process.env.POSTGRESQL_SERVICE_HOST;
+    dbPort = process.env.POSTGRESQL_SERVICE_PORT;
+}
+const client = new Client({
+    user: dbUser,
+    host: dbHost,
+    database: dbName,
+    password: dbPass,
+    port: dbPort
+})
+client.connect();
 
 var routes = require('./api/routes/userRoutes');
 routes(app);
@@ -11,3 +36,4 @@ app.listen(port);
 console.log('todo list RESTful API server started on: ' + port);
 
 module.exports = app;
+module.exports = client;
