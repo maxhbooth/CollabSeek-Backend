5334e25cfe1eb7837487d482c31c9aaee89c160a
Max Booth <mhb@live.unc.edu>
2018-02-20 23:57:50 -0500

    added express-validator and validated requests.

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,4 +1,5 @@
 var db = require('../../database/database')
+var expressValidator = require('express-validator');
 
 module.exports = function (app,client) {
     // set up the routes themselves
@@ -12,7 +13,39 @@ module.exports = function (app,client) {
         });
     });
 
+    app.post('/register', function(req, res) {
+
+        //validation
+        req.checkBody('username', 'Username must be between 4 and 15 characters.').len(4,15);
+        req.checkBody('email', 'Email must be a valid email.').isEmail();
+        req.checkBody('email', 'Email must be from 4 to 50 characters.').len(4,50);
+        req.checkBody('password', 'Password must be between 8 to 50 characters.').len(4,50);
+        req.checkBody('repassword', 'Passwords must match.').equals(password);
+
+        const errors = req.validationErrors();
+
+        if(errors) {
+            console.log(`errors: ${JSON.stringify(errors)}`);
+            res.send(errors);
+        }
+        else{
+
+            const username = req.body.username;
+            const email = req.body.email;
+            const password = req.body.password;
+
+            db.query('insert into users(username, email, password) \
+                 values (?, ?, ?)', [username, email, password], function(dberr, dbres, fields){
+                    if(dberr){
+                        throw dberr;
+                    }
+                 } )
+        }
+
+    })
+
     app.post('/login', function (req, res) {
         res.send('Hello world!')
     });
+
 };
