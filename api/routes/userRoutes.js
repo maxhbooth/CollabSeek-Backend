87845b1955b6f8a52c71c8f0e2c000ce5f639e34
Max Booth <mhb@live.unc.edu>
2018-02-21 00:41:59 -0500

    implemented validation

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -16,34 +16,36 @@ module.exports = function (app,client) {
     app.post('/register', function(req, res) {
         console.log('I ran')
         //validation
-        // req.checkBody('username', 'Username must be between 4 and 15 characters.').len(4,15);
-        // req.checkBody('email', 'Email must be a valid email.').isEmail();
-        // req.checkBody('email', 'Email must be from 4 to 50 characters.').len(4,50);
-        // req.checkBody('password', 'Password must be between 8 to 50 characters.').len(4,50);
-        // req.checkBody('repassword', 'Passwords must match.').equals(password);
+        //console.log(req.body.username);
+        req.checkBody('username', 'Username must be between 4 and 15 characters.').len(4,15);
+        req.checkBody('email', 'Email must be a valid email.').isEmail();
+        req.checkBody('email', 'Email must be from 4 to 50 characters.').len(4,50);
+        req.checkBody('password', 'Password must be between 8 to 50 characters.').len(4,50);
+        req.checkBody('repassword', 'Passwords must match.').equals(req.body.password);
 
-        const errors = 0; //req.validationErrors();
+        const errors = req.validationErrors();
 
         if(errors) {
-            console.log(errors);
+            console.log("found errors");
             //console.log(`errors: ${JSON.stringify(errors)}`);
             res.send(errors);
         }
         else{
 
-            console.log('no errors');
+             console.log('no errors');
 
-            // const username = req.body.username;
-            // const email = req.body.email;
-            // const password = req.body.password;
+            const username = req.body.username;
+            const email = req.body.email;
+            const password = req.body.password;
 
-            // db.query('insert into users(username, email, password) \
-            //      values (?, ?, ?)', [username, email, password], function(dberr, dbres, fields){
-            //         if(dberr){
-            //             throw dberr;
-            //         }
-            //      });
-        }
+             res.send("registration complete");
+        //     // db.query('insert into users(username, email, password) \
+        //     //      values (?, ?, ?)', [username, email, password], function(dberr, dbres, fields){
+        //     //         if(dberr){
+        //     //             throw dberr;
+        //     //         }
+        //     //      });
+         }
 
     });
 
--- server.js
+++ server.js
@@ -2,6 +2,15 @@ var express = require('express'),
 app = express(),
 port = process.env.PORT || 8080;
 
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+app.use(cookieParser());
+
 var routes = require('./api/routes/userRoutes');
 routes(app);
 
