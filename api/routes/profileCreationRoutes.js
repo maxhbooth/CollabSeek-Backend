e37ea869eb8aeedde7d9afe35cbe24e119cb27ca
Max Booth <mhb@live.unc.edu>
2018-03-03 12:58:12 -0500

    refactoring navbar

--- api/routes/profileCreationRoutes.js
+++ api/routes/profileCreationRoutes.js
@@ -8,6 +8,13 @@ const position = require('../../models/position');
 const skill = require('../../models/skill');
 const specialty = require('../../models/specialty');
 
+const profile_degree = require('../../models/profile_degree');
+const profile_department = require('../../models/profile_department');
+const profile_facility = require('../../models/profile_facility');
+const profile_skill = require('../../models/profile_skill');
+const profile_specialty = require('../../models/profile_specialty');
+
+
 module.exports = function (app, sessionChecker) {
 
     app.get('/create-profile', (req, res) => {
@@ -40,6 +47,9 @@ module.exports = function (app, sessionChecker) {
     });
     app.post('/create-profile', (req, res) => {
         if (req.session.profile && req.cookies.user_sid) {
+
+
+
 
 
         } else {
