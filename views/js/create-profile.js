16fcace7ae8cca4e5bc0fb0707595967c1456f5e
Alden <aldenc@live.unc.edu>
2018-03-05 23:44:10 -0500

    skills button

--- views/js/create-profile.js
+++ views/js/create-profile.js
@@ -1,1 +1,8 @@
-alert("hi");
+
+$(document).ready(function() {
+    var max_degrees = 6;
+
+    $("#add_degree").click(function(e){ //on add input button click
+            alert("yeah I havent done this yet");
+        })
+});
