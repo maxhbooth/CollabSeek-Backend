e471e39dfb6e8852e108df0d44810555da5d8bd3
Alden <aldenc@live.unc.edu>
2018-03-07 15:11:32 -0500

    Skeleton

--- views/js/create-profile.js
+++ views/js/create-profile.js
@@ -1,8 +1,47 @@
-
 $(document).ready(function() {
-    var max_degrees = 6;
+    var max_degrees = 10;
+    var current_degrees = 1;
 
     $("#add_degree").click(function(e){ //on add input button click
-            alert("yeah I havent done this yet");
-        })
+        if(current_degrees >= max_degrees){
+            $("#add_degree").remove();
+        }
+        current_degrees++;
+        var $newdegreeset = $('<div class="row" id="degset' + current_degrees.toString() + '"></div>');
+
+        // Degree choosing section
+        var $newdegree = $('<div class="form-group col-sm-4"></div>');
+        $newdegree.append('<label for=' + deg_id + '>Degree:</label>');
+        var $degreeselect = $('<select id=' + deg_id + ' name=' + deg_id + ' class="form-control"></select>');
+
+        var deg_id = "degree" + current_degrees.toString();
+        $("#degree option").each(function()
+        {
+            $degreeselect.append('<option>' + $(this).val() + '</option>');
+        });
+        $newdegree.append($degreeselect);
+
+        // Discipline choosing section
+        var $newdiscipline = $('<div class="form-group col-sm-4"></div>');
+        $newdiscipline.append('<label for=' + disc_id + '>Discipline:</label>');
+        var $disciplineselect = $('<select id=' + disc_id + 'name=' + disc_id + ' class="form-control"></select>');
+
+        var disc_id = "discipline" + current_degrees.toString();
+        $("#discipline option").each(function()
+        {
+            $disciplineselect.append('<option>' + $(this).val() + '</option>');
+        });
+        $newdiscipline.append($disciplineselect);
+
+        // Wrap up
+        $newdegreeset.append($newdegree);
+        $newdegreeset.append($newdiscipline);
+        $newdegreeset.append('<button type="button" id="degreedelete" class="btn btn-default">Delete</button>');
+        $("#degree_set").append($newdegreeset);
+    })
+
+    $(document).on('click', '#degreedelete', function () {
+        $(this).parent().remove();
+        current_degrees--;
+    });
 });
