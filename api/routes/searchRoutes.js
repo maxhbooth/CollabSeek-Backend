dd5c18e5a3c84bc59cf1eb683b585cd35e20a71c
macwatrous <macwatrous@gmail.com>
2018-03-28 16:20:27 -0400

    add advanced search and bug fix

--- api/routes/searchRoutes.js
+++ api/routes/searchRoutes.js
@@ -208,6 +208,7 @@ module.exports = function (app, sessionChecker) {
                         });
                     });
                 }
+                else doneSearch();
             }
 
             function doneSearch(){
