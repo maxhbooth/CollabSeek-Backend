ae4c42364f3e3222401f2bc2bfadcc3e9fb0da3f
Alden <aldenc@live.unc.edu>
2018-04-16 00:39:55 -0400

    profile style

--- api/routes/homeRoutes.js
+++ api/routes/homeRoutes.js
@@ -9,13 +9,7 @@ module.exports = function (app, sessionChecker) {
 
     app.get('/', (req, res) => {
         if (req.session.profile && req.cookies.user_sid) {
-            //let attributeRepository = new AttributeRepository();
-
-            //attributeRepository.getAll().then(function (models) {
-               // let searchData = models.departments.concat(models.disciplines, models.facilities, models.skills, models.specialties);
-                //res.render('homepage.html', {searchData: JSON.stringify(searchData)});
             res.render('homepage.html');
-            //});
         } else {
             res.sendFile('/views/welcome.html', {root: './'});
         }
@@ -34,28 +28,12 @@ module.exports = function (app, sessionChecker) {
         }
     });
 
-    // app.get('/profile', (req, res) => {
-    //     if (req.session.profile && req.cookies.user_sid) {
-    //
-    //         let profileRepositiory = new ProfileRepository();
-    //
-    //         profileRepositiory.getProfileInformation(req.session.profile.id).then(function (models){
-    //             //console.log(models);
-    //             res.render('profile.html', models);
-    //         });
-    //
-    //     } else {
-    //         res.redirect('/login');
-    //     }
-    // });
-
     app.get('/profile/:id', (req, res) => {
         if (req.session.profile && req.cookies.user_sid) {
             var id = req.params.id;
             let profileRepository = new ProfileRepository();
 
             profileRepository.getProfileInformation(id).then(function (models){
-                //console.log(models);
                 if(models != null){
                     res.render('profile.html', models);
                 }
@@ -133,9 +111,5 @@ module.exports = function (app, sessionChecker) {
 
 
             });
-
-
-
-
         });
 };
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -17,10 +17,6 @@ module.exports = function (app, sessionChecker) {
         return dest;
     }
 
-
-
-
-
     app.route('/login')
         .get(sessionChecker, (req, res) => {
             res.render('login.html');
@@ -90,8 +86,8 @@ module.exports = function (app, sessionChecker) {
 
                     //else send an email to change password
                     const html = 'Dear CollabSeek User, <br/><br/>  You are receiving this email because there was a request ' +
-                        'to change the account password for '+ user.first_name +user.last_name +
-                        'if this is true click the link below, if not just ignore this message.'+
+                        'to change the account password for '+ user.first_name + " " + user.last_name +
+                        '. If this is true click the link below, if not just ignore this message.'+
                         '<br/><br/><a href ="'+process.env.COLLAB_LINK+'/changepassword/'+password_token+'">'+process.env.COLLAB_LINK+'/changepassword/'+password_token+'</a>' +
                           '<br/><br/> Have a nice day, <br/> CollabSeek team';
                     mailer.sendEmail("collabuncseek@gmail.com", email, "Password Reset", html);
